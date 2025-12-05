aa037b3fa85cfeaa8c34dc0ea4fb9030dee39567
Prathamesh Kasar <Prathamesh.Kasar@bestbrains.com>
2025-12-05 17:49:31 +0530

    Env3 : Add GotId method to Science class

--- ScienceLibrary/Science.cs
+++ ScienceLibrary/Science.cs
@@ -15,5 +15,10 @@ namespace ScienceLibrary
             Object [] fields = { "Science has : ", physics.GetField(), biology.GetField(), chemistry.GetField() };
             return fields;
         }
+
+        public string GotId()
+        {
+            return "Checking that ScienceLibrary is updating or not..!!";
+        }
     }
 }
